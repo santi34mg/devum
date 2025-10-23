829859189b83bcc89c1fea571fb76f7e7072e039
santi34mg <ssilvera@correo.um.edu.uy>
2025-10-23 10:26:01 -0300

    Updates

--- assets/ui-vendor-BjyJ5RVQ.js
+++ assets/ui-vendor-BjyJ5RVQ.js
@@ -1,4 +1,4 @@
-import{r}from"./react-vendor-BQAv78yh.js";/**
+import{r}from"./react-vendor-COLTsl3e.js";/**
  * @license lucide-react v0.546.0 - ISC
  *
  * This source code is licensed under the ISC license.
